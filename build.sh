f739e98f08ea3baa6e844a19f6212fc366ac16ca
scuderia666 <satouuxd@gmail.com>
2022-07-19 00:40:27 +0300

    up

--- build.sh
+++ build.sh
@@ -1,13 +1,35 @@
 #!/bin/sh
 
+if [[ ! -d nya ]]; then
+	git clone https://github.com/fafnirlinux/nya
+fi
+
+if [[ ! -d nya ]]; then
+	exit 1
+fi
+
 cd nya
 mkdir -p build
 cd build
-if [[ ! -f Makefile ]]; then
-	cmake ..
-fi
+cmake ..
 make -j$(nproc)
 cp nya ../../pkg
 cd ../../
+
+if [[ ! -f pkg ]]; then
+	exit 1
+fi
+
+if [[ ! -d src ]]; then
+	mkdir -p src
+	cd src
+	git clone https://github.com/fafnirlinux/repo pkg
+	mv pkg/stuff .
+	cd ..
+fi
+
+if [[ ! -d src ]]; then
+	exit 1
+fi
 
 ./pkg $1 $2
