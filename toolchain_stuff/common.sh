fa9372dc54e2e82dd8c9ebc108afee7755625872
scuderia666 <satouuxd@gmail.com>
2022-07-27 12:57:50 +0300

    up

--- toolchain_stuff/common.sh
+++ toolchain_stuff/common.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+export CC=gcc
+export CXX=g++
+
+xconfflags=
+
+if [[ $STAGE = 0 ]]; then
+	export machine=$($CC -dumpmachine)
+	xconfflags="--host=$machine --target=$machine"
+fi
+
 inst() {
     local action=$@
     if [[ -z $action ]]; then
