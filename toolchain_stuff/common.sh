92cf3d0d4320b5a08a037c04fd2facb5afca24ff
scuderia666 <satouuxd@gmail.com>
2022-07-27 11:37:16 +0300

    up

---
+++ toolchain_stuff/common.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+inst() {
+    local action=$@
+    if [[ -z $action ]]; then
+        action="install"
+    fi
+    make DESTDIR=%dest $action
+}
+
+apply_patch() {
+    echo "applying patch $(basename $1)"
+    patch -p1 < $1
+}
